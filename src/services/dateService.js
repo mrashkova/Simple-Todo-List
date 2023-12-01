fb0354f76eb8d9a085aec37f86e00b50a978d0eb
Mariya Rashkova <mrashkkova@gmail.com>
2023-12-01 14:32:15 +0200

    add expired date function

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -1,8 +1,19 @@
-export const formatDate = () => {
+export const formatTodaysDate = () => {
   const today = new Date();
   const day = today.getDate();
   const month = today.getMonth() + 1;
   const year = today.getFullYear();
+
+  const fomatedDate = `${day}/${month}/${year}`;
+
+  return fomatedDate;
+};
+
+export const formatExpiredDate = () => {
+  const expired = new Date("November 28, 2023 14:00:00");
+  const day = expired.getDate();
+  const month = expired.getMonth() + 1;
+  const year = expired.getFullYear();
 
   const fomatedDate = `${day}/${month}/${year}`;
 
